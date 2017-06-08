1504e2f9f3bf293d96127181b59f4c8ee12d5af7
Wagner Faustino Vitor <w_vitor@yahoo.com.br>
2017-06-08 19:59:54 -0300

    Publicacao inicial
    Publicacao inicial

---
+++ TestePomodoro/TestePomodoro/TestePomodoro/Form1.Designer.cs
@@ -0,0 +1,166 @@
+﻿namespace TestePomodoro
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btnInicio = new System.Windows.Forms.Button();
+            this.btnDescanço = new System.Windows.Forms.Button();
+            this.lblCronomentro = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.btnStop = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnInicio
+            // 
+            this.btnInicio.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.btnInicio.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnInicio.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnInicio.Location = new System.Drawing.Point(12, 318);
+            this.btnInicio.Name = "btnInicio";
+            this.btnInicio.Size = new System.Drawing.Size(120, 50);
+            this.btnInicio.TabIndex = 0;
+            this.btnInicio.Text = "+ 1 Pomodoro";
+            this.btnInicio.UseVisualStyleBackColor = false;
+            this.btnInicio.Click += new System.EventHandler(this.btnInicio_Click);
+            // 
+            // btnDescanço
+            // 
+            this.btnDescanço.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.btnDescanço.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnDescanço.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDescanço.Location = new System.Drawing.Point(168, 318);
+            this.btnDescanço.Name = "btnDescanço";
+            this.btnDescanço.Size = new System.Drawing.Size(108, 50);
+            this.btnDescanço.TabIndex = 1;
+            this.btnDescanço.Text = "+1 Descanço";
+            this.btnDescanço.UseVisualStyleBackColor = false;
+            this.btnDescanço.Click += new System.EventHandler(this.btnDescanço_Click);
+            // 
+            // lblCronomentro
+            // 
+            this.lblCronomentro.AutoSize = true;
+            this.lblCronomentro.Font = new System.Drawing.Font("Lucida Handwriting", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCronomentro.Location = new System.Drawing.Point(177, 127);
+            this.lblCronomentro.Name = "lblCronomentro";
+            this.lblCronomentro.Size = new System.Drawing.Size(82, 31);
+            this.lblCronomentro.TabIndex = 2;
+            this.lblCronomentro.Text = "0000";
+            this.lblCronomentro.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // btnStop
+            // 
+            this.btnStop.BackColor = System.Drawing.Color.Red;
+            this.btnStop.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnStop.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnStop.Location = new System.Drawing.Point(305, 318);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(108, 50);
+            this.btnStop.TabIndex = 3;
+            this.btnStop.Text = "Parar";
+            this.btnStop.UseVisualStyleBackColor = false;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Lucida Handwriting", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(61, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(317, 27);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "Cronomentro Pomodoro";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Lucida Calligraphy", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Red;
+            this.label2.Location = new System.Drawing.Point(114, 97);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(223, 21);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "Contador em segundos:";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.pictureBox1.BackgroundImage = global::TestePomodoro.Properties.Resources.download;
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pictureBox1.ErrorImage = null;
+            this.pictureBox1.InitialImage = null;
+            this.pictureBox1.Location = new System.Drawing.Point(148, 161);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(147, 126);
+            this.pictureBox1.TabIndex = 13;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(435, 442);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.lblCronomentro);
+            this.Controls.Add(this.btnDescanço);
+            this.Controls.Add(this.btnInicio);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Cronomentro";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnInicio;
+        private System.Windows.Forms.Button btnDescanço;
+        private System.Windows.Forms.Label lblCronomentro;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ TestePomodoro/TestePomodoro/TestePomodoro/Form1.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TestePomodoro
+{
+    public partial class Form1 : Form
+    {
+        int minutos=25;
+        int soma=0;
+        int pausa = 5;
+
+     
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void btnInicio_Click(object sender, EventArgs e)
+        {
+
+            if (soma == 0)
+            {
+
+                soma = soma + (minutos * 60);
+                timer1.Interval = 1000;
+                timer1.Start();
+
+            }
+           
+            
+           
+            
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+
+            soma = soma -1;
+            lblCronomentro.Text = Convert.ToString(soma);
+            if(soma == 0)
+            {
+                timer1.Stop();
+            }
+      
+
+
+
+    }
+
+        private void btnDescanço_Click(object sender, EventArgs e)
+        {
+            if (soma == 0)
+            {
+                soma = soma + (pausa * 60);
+                timer1.Interval = 1000;
+                timer1.Start();
+
+
+
+            }
+            
+
+            
+
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+
+            timer1.Stop();
+            soma = 0;
+            lblCronomentro.Text = "0";
+        }
+      
+
+    }
+}
